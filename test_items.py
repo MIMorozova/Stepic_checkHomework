52c81babb834afd192a4473bce175b6e48ea9f17
MIMorozova <83887934+MIMorozova@users.noreply.github.com>
2021-05-13 23:23:04 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,9 +2,9 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_page(browser):
+def test_button_on_the_page(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     button = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary")
     assert button is not None, "this button is not found"
     
